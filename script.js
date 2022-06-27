f5b4b07144a315e06408295d346e134ca457b0f3
berkin <berkintuter@gmail.com>
2022-06-27 14:41:12 +0200

    first commit

--- script.js
+++ script.js
@@ -31,11 +31,9 @@ SEL=100;
 SUCRE=80;
 console.error("Nous avons du SEL en Q. de " + SEL + " Nous avons du sucre en quantité de " + SUCRE);
 
-SEL=80;
-SUCRE=100;
+SEL=SUCRE;
+
 console.error("Nous avons du SEL en Q. de " + SEL + " Nous avons du sucre en quantité de " + SUCRE);
 
 
-
-console.error("Nous avons du SEL en Q. de " + SUCRE + " Nous avons du sucre en quantité de " + SEL);
 
